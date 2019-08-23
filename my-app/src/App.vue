618b7ff3f47ded97ca3191542bbdd0451a885e2d
Xianfeng <jiaoxianfeng_0509@buaa.edu.cn>
2019-08-23 16:28:27 +0800

    'edit'

---
+++ my-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <router-view/>
+  </v-app>
+</template>
+
+<script>
+import NavBar from "./components/NavBar";
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
