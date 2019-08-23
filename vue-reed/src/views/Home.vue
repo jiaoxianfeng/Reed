7591b33145ee69f039f30c405a250e1bda9fc9d9
Xianfeng <jiaoxianfeng_0509@buaa.edu.cn>
2019-08-23 13:26:29 +0800

    'commit'

--- vue-reed/src/views/Home.vue
+++ vue-reed/src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
-    <Search/>
+    <!--<Search/>-->
+    <NavBar/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Search from '@/components/Search.vue'
+import NavBar from '@/components/NavBar.vue'
 
 export default {
   name: 'home',
   components: {
-    Search
+    NavBar
   }
 }
 </script>
