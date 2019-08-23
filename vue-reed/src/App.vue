8fda5f806a9c0b92db53d9d290ffda9cc2a06458
Xianfeng <jiaoxianfeng_0509@buaa.edu.cn>
2019-08-23 09:09:51 +0800

    vue test

--- vue-reed/src/App.vue
+++ vue-reed/src/App.vue
@@ -9,21 +9,21 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+  body {
+    background-color: #EEEEEE;
+    font-family: 'Montserrat', sans-serif;
+    display: grid;
+    grid-template-rows: auto;
+    justify-items: center;
+    align-items: center;
   }
-}
+  body, html {
+    margin: 0;
+    height: 100%;
+  }
+  #app {
+    width: 50%;
+  }
 </style>
--- vue-reed/src/main.js
+++ vue-reed/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VeeValidate from 'vee-validate'
 
 Vue.config.productionTip = false
+Vue.use(VeeValidate)
 
 new Vue({
   router,
