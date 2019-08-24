4a6fb368693d1d4615a8d3e92f7a1c5b0450ce3f
edwardtyrant <ruanyixuan1112@gmail.com>
2019-08-24 16:31:36 +0800

    newest update

--- src/components/SearchResultMenu.vue
+++ src/components/SearchResultMenu.vue
@@ -1,43 +1,30 @@
 <template>
     <div style="width: 90%; background-color: gray; margin-left: 5%; margin-right: 5%;">
-    <v-tabs
-            v-model="tab"
-            background-color="deep-purple accent-4"
-            class="elevation-2"
-            dark
-            :centered="centered"
-            :grow="grow"
-            :vertical="vertical"
-            :right="right"
-            :prev-icon="prevIcon ? 'mdi-arrow-left-bold-box-outline' : undefined"
-            :next-icon="nextIcon ? 'mdi-arrow-right-bold-box-outline' : undefined"
-            :icons-and-text="icons"
-    >
-        <v-tabs-slider></v-tabs-slider>
-        
-        <v-tab
-                v-for="i in tabs"
-                :key="i"
-                :href="`#tab-${i}`"
-        >
-            {{ tabNames[i-1] }}
-        </v-tab>
 
-        <v-tab-item
-                v-for="i in tabs"
-                :key="i"
-                :value="'tab-' + i"
-        >
-            <SearchResultBar
-                    v-for="bar in SearchResultBars[i-1]"
-                    :key="bar"
-                    :image="bar['image']"
-                    :title="bar['title']"
-                    :fstl="bar['fstl']"
-                    :scdl="bar['scdl']"
-            ></SearchResultBar>
-        </v-tab-item>
-    </v-tabs>
+        <v-tabs v-model="tab">
+            <v-tab disabled="true" style="color:black;"> 搜索结果</v-tab>
+            <v-tab v-for="i in tabs" :key="i" :href="`#tab-${i}`">
+                {{ tabNames[i-1] }}
+            </v-tab>
+            <v-tabs-slider></v-tabs-slider>
+        </v-tabs>
+
+        <v-tabs-items v-model="tab">
+            <v-tab-item
+                    v-for="i in tabs"
+                    :key="i"
+                    :value="'tab-' + i"
+            >
+                <SearchResultBar
+                        v-for="bar in SearchResultBars[i-1]"
+                        :key="bar"
+                        :image="bar['image']"
+                        :title="bar['title']"
+                        :fstl="bar['fstl']"
+                        :scdl="bar['scdl']"
+                ></SearchResultBar>
+            </v-tab-item>
+        </v-tabs-items>
     </div>
 </template>
 
@@ -51,13 +38,6 @@
         data () {
             return {
                 tab: null,
-                icons: false,
-                centered: false,
-                grow: false,
-                vertical: false,
-                prevIcon: false,
-                nextIcon: false,
-                right: false,
                 tabs: 3,
                 tabNames: ["书籍","影视","小组"],
             }
