904acb486dc7d0ef0614c2c656aa7297c46aad41
BKN46 <34993220+BKN46@users.noreply.github.com>
2019-08-25 10:08:58 +0800

    Update Comments.vue

--- my-app/src/components/Comments.vue
+++ my-app/src/components/Comments.vue
@@ -2,18 +2,19 @@
   <v-card
     class="mx-auto comments-card"
     color="#ccc"
-    max-width="500"
+    max-width="400"
     elevation="5"
+    style="border-radius: 0"
   >
 
     <v-card-actions>
       <v-list-item class="grow">
-        <v-list-item-avatar tile color="grey darken-3">
+        <v-list-item-avatar tile color="grey darken-3" style="width: 60px;height: 60px">
           <v-img
             class="elevation-6"
             src="../imgs/rotating_card_profile.png"
           ></v-img>
-      </v-list-item-avatar>
+        </v-list-item-avatar>
 
         <v-list-item-content class="user-name">
           <v-list-item-title class="user-name-text">Evan You</v-list-item-title>
@@ -32,7 +33,7 @@
           </v-list-item-title>
         </v-list-item-content>
 
-        <v-list-item-avatar tile color="grey darken-3">
+        <v-list-item-avatar tile color="grey darken-3" style="width: 50px;height: 50px">
           <v-img
             class="elevation-6 team-avatar"
             src="https://upload-images.jianshu.io/upload_images/2707438-61bec868c535b5d2.jpg?imageMogr2/auto-orient/"
@@ -47,10 +48,10 @@
        大家更喜欢什么风格的诗呢？
     </v-card-text>
 
-    <v-card-text class="headline text-content">
+    <v-card-text class="text-content">
       "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
     </v-card-text>
-    <v-card-actions>
+    <v-card-actions style="padding-bottom: 0px">
       <v-list-item class="grow">
 
         <v-row
@@ -60,7 +61,7 @@
         >
           <v-icon class="mr-1" color="white">mdi-comment</v-icon>
           <span class="subheading mr-2">256</span>
-          <span class="mr-1">·</span>
+          <span class="mr-1"></span>
           <v-icon class="mr-1" color="white">mdi-thumb-up</v-icon>
           <span class="subheading">45</span>
         </v-row>
@@ -109,20 +110,25 @@
   }
 
   .user-name-text{
-    font-size: 18px;
+    font-size: 24px;
   }
 
   .text-title{
-    font-size: 25px;
+    font-size: 20px;
     margin-bottom: -15px;
+    margin-top: -15px;
   }
 
   .text-content{
+    font-size: 14px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    margin-top: -15px;
     margin-bottom: -15px;
   }
 
   .comment-bottom{
     color: white;
-    margin-right: 20px;
+    margin-right: 0px;
   }
 </style>
