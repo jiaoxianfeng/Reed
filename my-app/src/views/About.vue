7da1cb36215eaedc01c912f87e6aacc7ba40686f
BKN46 <34993220+BKN46@users.noreply.github.com>
2019-08-28 11:28:15 +0800

    Sidebar+echarts

--- my-app/src/views/About.vue
+++ my-app/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <SideBar />
   </div>
 </template>
+
+<script>
+  import SideBar from "./SideBar";
+  export default {
+      name: 'About',
+      components:{
+          SideBar,
+      },
+      data() {
+
+      }
+  }
+</script>
