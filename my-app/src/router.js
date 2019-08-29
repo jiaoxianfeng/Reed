9b0d5530558faecf1c9433ff63220ac95a9f4b09
BKN46 <34993220+BKN46@users.noreply.github.com>
2019-08-29 15:08:51 +0800

    404 page

--- my-app/src/router.js
+++ my-app/src/router.js
@@ -87,6 +87,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "test" */ './views/BackManage.vue')
+    },
+    {
+      path: '*',
+      name: 'notFound',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "test" */ './views/NotFound.vue')
     }
   ]
 })
